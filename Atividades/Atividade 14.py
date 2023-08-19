55a6d3e635a2ceeacaf8a6a122f51b674ccff51a
Matheus <matheusrib0707@gmail.com>
2023-08-19 15:03:21 -0300

    Atualização

--- Atividades/Atividade 14.py
+++ Atividades/Atividade 14.py
@@ -1,1 +1,10 @@
 #Atividade 14
+#Programa para converter Celsius para Fahrenheit
+
+print("\nVamos converter Celsius para Fahrenheit\n")
+
+C = float(input("Digite a temperatura em Celsius para ser convertida: "))
+
+F = (C * 9/5) + 32
+
+print(f"\nA temperatura convertida é {F:.2f} Fahrenheit\n")
--- Atividades/Atividade 15.py
+++ Atividades/Atividade 15.py
@@ -1,1 +1,12 @@
 #Atividade 15
+#Aluguel de Carros sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado
+
+km = float(input("Digite quantos Km o carro rodou? "))
+dias = float(input("Digite quantos que o carro foi alugado? "))
+
+km = km * 0.15
+dias = dias * 60
+
+soma = km + dias
+
+print(f"O total do aluguel foi {soma:.2f}")
